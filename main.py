45a907ee78029d6dd68dd0532716dcad510b4542
Andrzej Wodecki <andrzej.wodecki@gmail.com>
2025-04-24 16:12:17 +0200

    sanity

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from langchain_community.document_loaders import TextLoader
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
