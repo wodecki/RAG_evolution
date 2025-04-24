774f4aed4b32377fb01f16ea0dbea784f2a3cc55
Andrzej Wodecki <andrzej.wodecki@gmail.com>
2025-04-24 11:36:07 +0200

    WebBaseLoader implemented

--- main.py
+++ main.py
@@ -2,17 +2,32 @@ from langchain_community.document_loaders import TextLoader
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
+import bs4
+# Load documents from www
+from langchain_community.document_loaders import WebBaseLoader
+loader = WebBaseLoader(
+    web_paths=(
+        "https://en.wikipedia.org/wiki/Albert_Einstein",
+        "https://en.wikipedia.org/wiki/Marie_Curie",
+        "https://en.wikipedia.org/wiki/Charles_Darwin",
+        "https://en.wikipedia.org/wiki/Ada_Lovelace",
+        "https://en.wikipedia.org/wiki/Isaac_Newton"
+    ),
+    # Corrected bs_kwargs using the ID for the main content div
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            id="bodyContent" # Use the ID selector for Wikipedia's main content area
+        )
+    ),
+)
+docs = loader.load()
 
-# Load documents from data directory
-file_path = "./datasets/scientists_bios/Ada Lovelace.txt"
-loader = TextLoader(file_path)
-doc = loader.load()
 
 # Create embeddings and vector store
 embeddings = OpenAIEmbeddings()
 vector_store = InMemoryVectorStore(embeddings)
 
-vector_store.add_documents(documents=doc)
+vector_store.add_documents(documents=docs)
 
 # Define the retriever
 retriever = vector_store.as_retriever()
